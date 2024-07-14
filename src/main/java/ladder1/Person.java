6f2a1598799840f95c966d79ef3fc0c937c4bba4
yuho9 <catch919@gmail.com>
2024-07-14 21:22:47 +0900

    이름 5자 초과 오류처리

--- src/main/java/ladder1/Person.java
+++ src/main/java/ladder1/Person.java
@@ -5,6 +5,9 @@ public class Person {
 private String name;
 	
 	public Person(String name){
+		if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
 		this.name = name;
 	}
 }
